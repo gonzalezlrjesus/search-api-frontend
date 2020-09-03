b16d19233f99e6d320783e35cdda66f7d74db657
gonzalezlrjesus <gonzalezlrjesus@gmail.com>
2020-09-03 06:26:22 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test1';
+
+usersArray: Array<any> = [];
+checkoutForm: FormGroup;
+
+constructor(private formBuilder: FormBuilder,private http: HttpClient) { 
+  this.checkoutForm = this.formBuilder.group({
+    name: '',
+  });
+}
+
+onSubmit(value) {
+
+  this.usersArray = [];
+  let urlGET = `https://search-api-tribal.herokuapp.com/v1/search?query=${value.name}`;
+
+  this.http.get<any[]>(urlGET).subscribe(data => {
+    data["query"].forEach(element => {
+      this.usersArray.push({name: element.name, kind: element.kind, date: element.date, originapi: element.originapi});
+    });
+  });
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+import {MatGridListModule} from '@angular/material/grid-list'; 
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
